1803f17dd548f0aa75b0be00d7d661130ad2e801
abdullah-motasim <abdullah.motasim@gmail.com>
2024-09-27 11:27:36 -0400

    Removed unnessecary data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -15,10 +15,11 @@ library(dplyr)
 raw_data <- read_csv("data/raw_data/raw_ambulance_response_data.csv")
 
 cleaned_data <- suppressWarnings(
-  raw_data |>
+  raw_data[,2:5] |>
   janitor::clean_names() |>
-  # Remove unneeded columns which describe data types
-  select(-field_name, -desctiption_definition, -comments_examples) |>
+  # Remove columns which are not used in analysis
+  # select(-forward_sortation_area, -field_name, -desctiption_definition, -comments_examples) |>
+    
   na.omit() |>
   # Convert date and time variables
   separate(col = dispatch_time,
@@ -39,10 +40,10 @@ cleaned_data <- suppressWarnings(
     second = ifelse(is.na(second), "00", second)) |>
   # create a new date column
   mutate(dispatch_time = lubridate::ymd_hms(paste(year, month, day, hour, minute, second, sep = "-"))) |>
-  # Remove unneeded columns which describe data types
-  select(-hour, -minute, -second, -day) |>
+  # Remove unneeded columns which describe date
+  select(-hour, -minute, -second, -day, -month) |>
   #Reorder columns
-  select(id, dispatch_time, incident_type, priority_number, units_arrived_at_scene, forward_sortation_area, everything()))
+  select(dispatch_time, incident_type, priority_number, units_arrived_at_scene, year))
 
 #### Test data ####
 
@@ -50,16 +51,15 @@ cleaned_data <- suppressWarnings(
 paste("Negative Number of Dispatchers Test: ", cleaned_data$units_arrived_at_scene |> min() < 0)
 
 # test for dates within range specified
-dates <- c(2017, 2018, 2019, 2020, 2021, 2022)
-paste("Dispatch Time out-of-range Test: ", any(!cleaned_data$dispatch_time %in% priority_levels))
+years <- c(2017, 2018, 2019, 2020, 2021, 2022)
+paste("Dispatch Time out-of-range Test: ", any(!cleaned_data$year %in% years))
 
 # test for priority level meets requirements
 priority_levels <- c(1, 3, 4, 5, 9, 11, 12, 13, 14)
 paste("Priority Level Outside of Range Test: ", any(!cleaned_data$priority_number %in% priority_levels))
 
 # test for missing values
-any_NA_values <- all(is.na(cleaned_data$id))
-any_NA_values <- all(is.na(cleaned_data$date))
+any_NA_values <- all(is.na(cleaned_data$dispatch_time))
 any_NA_values <- all(is.na(cleaned_data$incident_type))
 any_NA_values <- all(is.na(cleaned_data$priority_number))
 any_NA_values <- all(is.na(cleaned_data$units_arrived_at_scene))
